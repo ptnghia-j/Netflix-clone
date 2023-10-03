d526e40868dc7982fb6edc852b0c2fa0b5ec80a5
Nghia T Phan <ptnghia@csu.fullerton.edu>
2023-10-03 16:08:22 -0700

    Profile screen added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
 
-const inter = Inter({ subsets: ['latin'] })
+import useCurrentUser  from '@/hooks/useCurrentUser'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const { data: user} = useCurrentUser();
+
   return (
-    <h1 className='text-2xl text-green-500'> Netflix clone</h1>
+    <>
+      <h1 className='text-4xl text-green-500'> Netflix clone</h1>
+      <p className="text-white"> Logged in in as: {user?.email} </p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}> Log out! </button>
+    </>
   )
 }
