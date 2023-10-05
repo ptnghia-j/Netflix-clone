b4b3e1d20377efa13bd4b9f56bb713a8a02ff44d
Nghia T Phan <ptnghia@csu.fullerton.edu>
2023-10-05 16:44:03 -0700

    Nav Bar & components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from 'next'
 import { getSession, signOut } from 'next-auth/react'
+import NavBar from '@/components/Navbar'
 
 import useCurrentUser  from '@/hooks/useCurrentUser'
 
@@ -25,9 +26,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className='text-4xl text-green-500'> Netflix clone</h1>
-      <p className="text-white"> Logged in in as: {user?.email} </p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}> Log out! </button>
+      <NavBar />
     </>
   )
 }
